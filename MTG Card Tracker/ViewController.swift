20071446ca7b96dd421f7120598a74aea7b25dcc
ihanken <ihanken@bellsouth.net>
2018-03-21 23:59:11 -0500

    Added a bunch of stuff. travis.yml, Dangerfile, etc.

---
+++ MTG Card Tracker/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MTG Card Tracker
+//
+//  Created by Ian Hanken on Wednesday, March 21, 2018.
+//  Copyright © 2018 Froghat LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ fastlane/travis.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+    fastlane test --verbose
+    exit $?
+fi
+
+if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
+    fastlane beta --verbose
+    exit $?
+fi
+
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    fastlane release --verbose
+    exit $?
+fi
